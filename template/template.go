4d01f77d48efa7b6de1ffa38453265097d8cc848
simmo1010 <ian.simpson.nz@gmail.com>
2015-10-17 21:00:11 +1300

    Parsefiles

--- template/template.go
+++ template/template.go
@@ -4,16 +4,48 @@ package main
 import (
 	"log"
 	"os"
-	"text/template"
+	"html/template"
+	"fmt"
 )
 
+type Page struct {
+	Title	string
+	Body		string
+}
+
 func main() {
-	tpl, err := template.ParseFiles("tpl.gohtml")
+	
+	tpl, err := template.ParseFiles("tpl.gohtml", "tpl2.gohtml")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	err = tpl.Execute(os.Stdout, []int{1, 2, 3, 4, 5})
+	
+	err = tpl.Execute(os.Stdout, Page{
+		Title:	"Which file?",
+		Body:	"hello page 1",
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	
+	fmt.Println("\n***********************")
+	
+	err = tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", Page{
+		Title:	"specifying tpl.gohtml",
+		Body:	"hello page 1",
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	
+		fmt.Println("\n***********************")
+	
+	err = tpl.ExecuteTemplate(os.Stdout, "tpl2.gohtml", Page{
+		Title:	"specifying tpl2.gohtml",
+		Body:	"hello page 2",
+	})
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
+
