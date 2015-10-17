64b3553b19503d048248084b0b29eaeb9d29fe0a
simmo1010 <ian.simpson.nz@gmail.com>
2015-10-17 18:23:50 +1300

    Add template

---
+++ template/template.go
@@ -0,0 +1,19 @@
+// template
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+func main() {
+	tpl, err := template.ParseFiles("tpl.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = tpl.Execute(os.Stdout, 5*5)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
